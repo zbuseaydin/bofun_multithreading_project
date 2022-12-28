e111e4e47d1135fc7b7b47415f4a0ff67edc3d4a
zbuseaydin <zeynpaydinn@gmail.com>
2022-12-29 01:09:03 +0300

    queue implementation

--- new.cpp
+++ new.cpp
@@ -6,31 +6,29 @@
 #include <sstream>
 #include <unistd.h>
 #include <queue>
+#include <string.h>
 using namespace std;
 
 pthread_mutex_t companyMutexes[5];
-pthread_mutex_t customerMutexes[10];
+pthread_mutex_t vtmMutexes[10];
 pthread_mutex_t finishedMutex;
+pthread_mutex_t outputLock;
+
 vector<string> inputLines;
-bool check = false;
+vector<bool> prepaymentDone;
+
+int companies[5];
+
 int finished;
-class Customer{
-    public:
-        int amount;
-        string company;
-        bool prepaymentDone;
-        int customerNo;
-        Customer(int a, string c, int cusNo){
-            amount = a;
-            company = c;
-            prepaymentDone = false;
-            customerNo = cusNo;
-        }
-        Customer(){
-            prepaymentDone = false;
-        }
+ofstream output;
+
+struct customer{
+    int amount;
+    string company;
+    int customerNo;
 };
-queue<Customer> vendingQueues[10];
+
+queue<customer> vendingQueues[10];
 
 int kevin = 0;
 int bob = 0;
@@ -46,16 +44,19 @@ int main(int argc, char *argv[]){
     int numCustomers;
     ifstream input(argv[1]);
 
+    output.open("logFile");
+
     input >> numCustomers;
     finished = numCustomers;
 
     for(int i; i < 10; i++)
-        pthread_mutex_init(&customerMutexes[i], 0);
+        pthread_mutex_init(&vtmMutexes[i], 0);
 
     for(int i=0; i<5; i++)
         pthread_mutex_init(&companyMutexes[i], 0);
 
     pthread_mutex_init(&finishedMutex, 0);
+    pthread_mutex_init(&outputLock, 0);
 
     int *itr;
     pthread_t vendingIds[10];
@@ -77,6 +78,9 @@ int main(int argc, char *argv[]){
         inputLines.push_back(line);
     }
 
+    for(int i=0; i<numCustomers; i++)
+        prepaymentDone.push_back(false);
+
     // create customer processes
     for (int i=0; i<numCustomers; i++)
     {
@@ -92,76 +96,87 @@ int main(int argc, char *argv[]){
         pthread_join(vendingIds[i], NULL);
     
     for (int i = 0; i<10; i++)
-        pthread_mutex_destroy(&customerMutexes[i]);
+        pthread_mutex_destroy(&vtmMutexes[i]);
     for (int i = 0; i<5; i++)
         pthread_mutex_destroy(&companyMutexes[i]);
     pthread_mutex_destroy(&finishedMutex);
+    pthread_mutex_destroy(&outputLock);
 
-    cout << "All prepayments are completed.\n";
-    cout << "Kevin: ";
-    cout << kevin;
-    cout << "TL\n";
-    cout << "Bob: ";
-    cout << bob;
-    cout << "TL\n";
-    cout << "Stuart: ";
-    cout << stuart;
-    cout << "TL\n";
-    cout << "Otto: ";
-    cout << otto;
-    cout << "TL\n";
-    cout << "Dave: ";
-    cout << dave;
-    cout << "TL\n";
+    output << "All prepayments are completed.\n";
+    output << "Kevin: ";
+    output << kevin;
+    output << "TL\n";
+    output << "Bob: ";
+    output << bob;
+    output << "TL\n";
+    output << "Stuart: ";
+    output << stuart;
+    output << "TL\n";
+    output << "Otto: ";
+    output << otto;
+    output << "TL\n";
+    output << "Dave: ";
+    output << dave;
+    output << "TL\n";
 }
 
 void* processInfo(void *itr)
 {
     vector<string> infoList;
     string info;
-    stringstream line(inputLines[*(int*)itr]);
+    int cusNo = *(int*)itr;
+    stringstream line(inputLines[cusNo]);
 
     while(getline(line, info, ',')){
         infoList.push_back(info);
     }
     string company = infoList[2];
     int amount = stoi(infoList[3]);
-    cout << "Customer";
-    cout << *(int*)itr+1;
-    cout << ",";
-    cout << amount;
-    cout << "TL,";
-    cout << company;
-    cout << endl;
-
+    
     int sleepTime = stoi(infoList[0]);
     sleep(sleepTime/1000);
     int vendingMachine = stoi(infoList[1])-1;
-    pthread_mutex_lock(&customerMutexes[vendingMachine]);
-    Customer cus(amount, company, *(int*) itr);
-    vendingQueues[vendingMachine].push(cus);
-    pthread_mutex_unlock(&customerMutexes[vendingMachine]);
-   // while(!cus.prepaymentDone);
-    cout << "exiting customer thread" << *(int*)itr << "\n";
 
+    pthread_mutex_lock(&vtmMutexes[vendingMachine]);
+    customer cus;
+    cus.amount = amount;
+    cus.company = company;
+    cus.customerNo = cusNo;
+    vendingQueues[vendingMachine].push(cus);
+    pthread_mutex_unlock(&vtmMutexes[vendingMachine]);
+
+    while(!prepaymentDone[cusNo]);
+    
     pthread_exit(0);
 }
 
 void* paymentRoutine(void *itr)
 {
     int vmNo = *(int*) itr;
+
     while(true)
     {
         if(!vendingQueues[vmNo].empty()){
-            pthread_mutex_lock(&customerMutexes[vmNo]);
+            pthread_mutex_lock(&vtmMutexes[vmNo]);
 
             int amount = vendingQueues[vmNo].front().amount;
             string company = vendingQueues[vmNo].front().company;
-            vendingQueues[vmNo].front().prepaymentDone = true;
-
+            int customerNo = vendingQueues[vmNo].front().customerNo;
             vendingQueues[vmNo].pop();
             performPayment(company, amount);
-            pthread_mutex_unlock(&customerMutexes[vmNo]);
+            prepaymentDone[customerNo] = true;
+            pthread_mutex_unlock(&vtmMutexes[vmNo]);
+
+            pthread_mutex_lock(&outputLock);
+            output << "Customer";
+            output << customerNo;
+            output << ",";
+            output << amount;
+            output << "TL,";
+            output << company;
+            output << endl;
+            pthread_mutex_unlock(&outputLock);
+
             pthread_mutex_lock(&finishedMutex);
             finished--;    
             pthread_mutex_unlock(&finishedMutex);
@@ -171,65 +186,30 @@ void* paymentRoutine(void *itr)
         }
         
     }
+    pthread_mutex_lock(&outputLock);
     cout << "exiting..";
     cout << vmNo;
     cout << endl;
-    free(itr);
+    pthread_mutex_unlock(&outputLock);
+
     pthread_exit(0);
 }
 
 void performPayment(string company, int amount)
 {   
+    int companyNo;
     if(company == "Kevin")
-    {
-        pthread_mutex_lock(&companyMutexes[0]);
-        kevin += amount;
-        cout << "Kevin: ";
-        cout << kevin;
-        cout << endl;
-        pthread_mutex_unlock(&companyMutexes[0]);
-    }
-    else if(company == "Bob")
-    {
-        pthread_mutex_lock(&companyMutexes[1]);
-        bob += amount;
-        cout << "Bob: ";
-        cout << bob;
-        cout << endl;
-        pthread_mutex_unlock(&companyMutexes[1]);
-    }
+        companyNo = 0;
+    else if (company == "Bob")
+        companyNo = 1;
     else if(company == "Stuart")
-    {
-        pthread_mutex_lock(&companyMutexes[2]);
-        stuart += amount;
-        cout << "Stuart: ";
-        cout << stuart;
-        cout << endl;
-        pthread_mutex_unlock(&companyMutexes[2]);
-    }
+        companyNo = 2;
     else if(company == "Otto")
-    {
-        pthread_mutex_lock(&companyMutexes[3]);
-        otto += amount;
-        cout << "Otto: ";
-        cout << otto;
-        cout << endl;
-        pthread_mutex_unlock(&companyMutexes[3]);
-    }
+        companyNo = 3;
     else if(company == "Dave")
-    {
-        pthread_mutex_lock(&companyMutexes[4]);
-        dave += amount;
-        cout << "Dave: ";
-        cout << dave;
-        cout << endl;
-        pthread_mutex_unlock(&companyMutexes[4]);
-    }
-    else
-    {
-        cout << company;
-        cout << "Company name not found.";
-        exit(1);
-    }
+        companyNo = 4;
+    pthread_mutex_lock(&companyMutexes[companyNo]);
+    companies[companyNo] += amount;
+    pthread_mutex_unlock(&companyMutexes[companyNo]);
 
 }
